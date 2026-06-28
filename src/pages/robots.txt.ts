a966ee6cb1c90fd8d3b8f41910a054bffc9cdd18
KyberYbk <rechuta200344@gmail.com>
2026-06-28 19:28:49 +0600

    feat: configure robots.txt and sitemap

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,8 +1,16 @@
 import type { APIRoute } from "astro";
 
-export const GET: APIRoute = ({ url }) => {
-  const origin = `${url.protocol}//${url.host}`;
-  const body = `User-agent: *\nAllow: /\n\nSitemap: ${origin}/sitemap.xml\n`;
+export const GET: APIRoute = ({ site }) => {
+  const origin = site?.origin ?? "https://swageda.ru";
+  const body = [
+    "User-agent: *",
+    "Allow: /",
+    "Disallow: /random",
+    "Disallow: /favorites",
+    "",
+    `Sitemap: ${origin}/sitemap.xml`,
+    "",
+  ].join("\n");
   return new Response(body, {
     headers: { "Content-Type": "text/plain; charset=utf-8" },
   });
